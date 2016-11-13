f6ee17d36444ac403e43b59ad740cb1e752a97c7
Simon Wåhlin <simon.wahlin@gmail.com>
2016-11-13 10:00:35 +0100

    Added sample DSC configuration for creating a new AD Forest

---
+++ CreateForest.ps1
@@ -0,0 +1,47 @@
+Configuration Main
+{
+
+	Param ( [string] $nodeName, [string] $DomainName, [PSCredential] $DomainCreds )
+
+	Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory, xStorage
+
+	Node localhost
+	{
+		LocalConfigurationManager 
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+		xDisk InitDataDisk
+		{
+			DriveLetter = 'F:'
+			DiskNumber = 2
+			FSFormat = 'NTFS'
+		}
+
+		xWaitforDisk WaitForDataDisk
+        {
+            DiskNumber = 2
+            RetryIntervalSec = 10
+            RetryCount = 60
+        }
+
+		WindowsFeature ADDS
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"		
+			DependsOn = "[xWaitforDisk]WaitForDataDisk"
+        }
+		xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+	        DependsOn = "[WindowsFeature]ADDS"
+        }
+	}
+}
